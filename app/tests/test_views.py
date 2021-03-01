bdef99cfdf161f55b4a06808b6cfa675f2ec91f5
MarlineKhavele <marlinekhavele6@gmail.com>
2021-03-01 23:32:40 +0300

    tests

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,19 +1,16 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
-from rest_framework import status
-from django.urls import reverse
-from django.contrib.auth.models import User
 
-
-# Define this after the ModelTestCase
-class ViewTestCase(TestCase):
+class ViewsTestCase(TestCase):
     """Test suite for the api views."""
 
     def setUp(self):
         """Define the test client and other test variables."""
         user = User.objects.create(username="nerd")
+
+        # Initialize client and force it to use authentication
         self.client = APIClient()
         self.client.force_authenticate(user=user)
+
+        # Since user model instance is not serializable, use its Id/PK
         self.bucketlist_data = {'name': 'Go to Ibiza', 'owner': user.id}
         self.response = self.client.post(
             reverse('create'),
@@ -28,7 +25,7 @@ class ViewTestCase(TestCase):
         """Test that the api has user authorization."""
         new_client = APIClient()
         res = new_client.get('/bucketlists/', kwargs={'pk': 3}, format="json")
-        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_api_can_get_a_bucketlist(self):
         """Test the api can get a given bucketlist."""
@@ -50,7 +47,6 @@ class ViewTestCase(TestCase):
         )
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
-
     def test_api_can_delete_bucketlist(self):
         """Test the api can delete a bucketlist."""
         bucketlist = Bucketlist.objects.get()
@@ -58,5 +54,4 @@ class ViewTestCase(TestCase):
             reverse('details', kwargs={'pk': bucketlist.id}),
             format='json',
             follow=True)
-
         self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
